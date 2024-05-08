eb4def931f0062f72bc5d71c1170a719273c7b52
DavideBernasconi13 <158482056+DavideBernasconi13@users.noreply.github.com>
2024-05-08 16:45:51 +0200

    Update footer.php

--- Views/footer.php
+++ Views/footer.php
@@ -1,5 +1,51 @@
 <footer>
-    <p class="text-center">Copyright &copy; 2024</p>
+   <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <h3>Assistenza</h3>
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h3>Mio account</h3>
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h3>Info</h3>
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h3>Contattaci</h3>
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                    <li><a href="#">Link 5</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-credits">
+        <div class="container text-center">
+            &copy;2024 | Gruppo 2: Nazario Zaccagnino, Nicolai Barbarasa, Manuel Maisano, Gabriele Suma, Bernasconi
+            Davide
+        </div>
+    </div>
 </footer>
 
 </body>
