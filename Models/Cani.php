b0a6aee189157a8ffa6934a2554e44918b237852
Nicolae <nicolaibarbarasa@gmail.com>
2024-05-08 16:48:50 +0200

    add cani.php

---
+++ Models/Cani.php
@@ -0,0 +1,26 @@
+<?php
+include_once __DIR__ . '/Product.php';
+class Dogs extends Product
+{
+    
+    public function __construct($id, $name, $weight, $price, $img, $category)
+    {
+        parent::__construct($id, $name, $weight, $price, $img, $category);
+        
+
+
+    }
+    public function formatItem()
+    {
+        $item = [
+            'name' => $this->name,
+            'weight' => $this->weight,
+            'price' => $this->price,
+            'img' => $this->img,
+            'category' => $this->category
+        ];
+        return $item;
+    }
+
+
+}
