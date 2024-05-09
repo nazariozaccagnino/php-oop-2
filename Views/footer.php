54ee0fb8fe5771af63daa0cf7c698265698acba1
Manuel Maisano <manumaisano@gmail.com>
2024-05-09 15:17:33 +0200

    footer icon

--- Views/footer.php
+++ Views/footer.php
@@ -2,41 +2,41 @@
     <div class="container">
         <div class="row">
             <div class="col-3">
-                <h3>Assistenza</h3>
+                <h4>ASSISTENZA</h4>
                 <ul>
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                    <li><a href="#">Link 4</a></li>
+                    <li><a href="#">Spedizioni</a></li>
+                    <li><a href="#">Punti di ritiro</a></li>
+                    <li><a href="#">Pagamenti</a></li>
+                    <li><a href="#">Faqs</a></li>
                 </ul>
             </div>
             <div class="col-3">
-                <h3>Mio account</h3>
+                <h4>MIO ACCOUNT</h4>
                 <ul>
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                    <li><a href="#">Link 4</a></li>
+                    <li><a href="#">I Miei Ordini</a></li>
+                    <li><a href="#">I Miei Indirizzi</a></li>
+                    <li><a href="#">I Mei Punti Fedelta</a></li>
+                    <li><a href="#">Lista Desideri</a></li>
                 </ul>
             </div>
             <div class="col-3">
-                <h3>Info</h3>
+                <h4>INFORMAZIONI</h4>
                 <ul>
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                    <li><a href="#">Link 4</a></li>
-                    <li><a href="#">Link 5</a></li>
+                    <li><a href="#">La Famiglia PetStore</a></li>
+                    <li><a href="#">Condizioni Generali</a></li>
+                    <li><a href="#">Diritto di Recesso</a></li>
+                    <li><a href="#">Privacy e Cookie Policy</a></li>
+                    <li><a href="#">Codice di Condotta</a></li>
                 </ul>
             </div>
             <div class="col-3">
-                <h3>Contattaci</h3>
+                <h4>CONTATTACI</h4>
                 <ul>
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                    <li><a href="#">Link 4</a></li>
-                    <li><a href="#">Link 5</a></li>
+                    <li><a href="#"><i class="fa-solid fa-location-dot"></i> PetStore S.r.l., Via Piemonte 4 - 63100 Ascoli Piceno</a></li>
+                    <li><a href="#"> <i class="fa-solid fa-phone"></i> Contattaci subito: 0736250484 INT.4</a></li>
+                    <li><a href="#"><i class="fa-solid fa-envelope"></i> Email: petstore@petstore.it</a></li>
+                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i> WhatsApp: 379 196 9643</a></li>
+                   
                 </ul>
             </div>
         </div>
